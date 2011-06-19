3177256f9c433bf3efed73ee2ece12aff8f94431
Vlad Toader <vladtoader@gmail.com>
2011-06-19 12:34:54 -0500

    Exercise 17 - Swipey - initial modifications

---
+++ Exercise15-Navigtor/Exercise15-Navigtor/SwipeyViewController.h
@@ -0,0 +1,16 @@
+//
+//  SwipeyViewController.h
+//  Exercise15-Navigtor
+//
+//  Created by Vlad Toader on 6/19/11.
+//  Copyright 2011 Progressive Insurance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SwipeyViewController : UIViewController {
+    
+}
+
+@end
