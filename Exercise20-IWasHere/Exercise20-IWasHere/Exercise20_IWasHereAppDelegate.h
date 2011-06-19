1284dfc1f4346d9c5a009738e7df64c261e47cf6
Vlad Toader <vladtoader@gmail.com>
2011-06-19 15:59:47 -0500

    added Exercise20-IWasHere

---
+++ Exercise20-IWasHere/Exercise20-IWasHere/Exercise20_IWasHereAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  Exercise20_IWasHereAppDelegate.h
+//  Exercise20-IWasHere
+//
+//  Created by Vlad Toader on 6/19/11.
+//  Copyright 2011 Progressive Insurance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Exercise20_IWasHereAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
