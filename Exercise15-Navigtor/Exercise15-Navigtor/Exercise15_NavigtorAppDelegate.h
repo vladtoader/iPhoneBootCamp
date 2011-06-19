4ceb502dd9b1ccc8d1ee4c271f02e2be3cc9ed69
Vlad Toader <vladtoader@gmail.com>
2011-06-19 10:24:01 -0500

    added Exercise 15

---
+++ Exercise15-Navigtor/Exercise15-Navigtor/Exercise15_NavigtorAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Exercise15_NavigtorAppDelegate.h
+//  Exercise15-Navigtor
+//
+//  Created by Vlad Toader on 6/19/11.
+//  Copyright 2011 Progressive Insurance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Exercise15_NavigtorAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ Exercise15-Navigtor/Exercise15-Navigtor/FirstLevelViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstLevelViewController.h
+//  Exercise15-Navigtor
+//
+//  Created by Vlad Toader on 6/19/11.
+//  Copyright 2011 Progressive Insurance. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface FirstLevelViewController : NSObject {
+    
+}
+
+@end
