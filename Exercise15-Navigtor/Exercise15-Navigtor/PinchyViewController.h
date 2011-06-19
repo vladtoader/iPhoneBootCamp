281b7aa0dcf097e96e51b4b23c36f18ba825c4bf
Vlad Toader <vladtoader@gmail.com>
2011-06-19 12:53:39 -0500

    Exercise 18 - Pinchy - start

---
+++ Exercise15-Navigtor/Exercise15-Navigtor/PinchyViewController.h
@@ -0,0 +1,16 @@
+//
+//  PinchyViewController.h
+//  Exercise15-Navigtor
+//
+//  Created by Vlad Toader on 6/19/11.
+//  Copyright 2011 Progressive Insurance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PinchyViewController : UIViewController {
+    
+}
+
+@end
